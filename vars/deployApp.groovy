845fe54fe6adcd186a86d0923ad8c9eff2395827
Karthik994921 <91363380+Karthik994921@users.noreply.github.com>
2024-07-22 10:51:10 +0530

    Update deployApp.groovy
    added info

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,1 +1,8 @@
+def call(String warFile, String tomcatHost, String tomcatPort, String tomcatCredentialsId) {
+    withCredentials([usernamePassword(credentialsId: tomcatCredentialsId, passwordVariable: 'TOMCAT_PASS', usernameVariable: 'TOMCAT_USER')]) {
+        sh """
+        curl --upload-file ${warFile} "http://${TOMCAT_USER}:${TOMCAT_PASS}@${tomcatHost}:${tomcatPort}/manager/text/deploy?path=/your_app_context&update=true"
+        """
+    }
+}
 
